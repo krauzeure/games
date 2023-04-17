b7142451a3a482339592897350689dea40eef813
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-04-17 18:43:09 +0200

    Installing react router dom and creating our basic route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css';
-import Homepage from './Pages/Homepage/Homepage';
+import './App.css'
+import Homepage from './Pages/Homepage/Homepage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div className='App'>
+      <Routes>
+        <Route path="" element={<Homepage />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
