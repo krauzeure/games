03df68bbeca277fdca12b74745b0f0e19c72662a
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-04-17 19:24:18 +0200

    Adding popular games block

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,11 +1,23 @@
 import './Homepage.css'
+import Memory from './Memory.svg'
 
 export default function Homepage() {
   return (
-    <main className="homepage-main">
-        <h1>Jeux en ligne</h1>
-        <p>Venez découvrir nos mini-jeux en ligne. Les jeux sont gratuits et sont accessibles sans inscription !</p>
-        <div className="link">Découvrir les jeux</div>
+    <main className='homepage-main'>
+      <h1>Jeux en ligne</h1>
+      <p>
+        Venez découvrir nos mini-jeux en ligne. Les jeux sont gratuits et sont accessibles sans
+        inscription !
+      </p>
+      <section className='popular-games'>
+        <h2>Nos jeux les plus populaires</h2>
+        <div className='popular-games-block'>
+          <img src={Memory} alt='memory' />
+          <img src={Memory} alt='memory' />
+          <img src={Memory} alt='memory' />
+        </div>
+      </section>
+      <div className='link'>Découvrir les jeux</div>
     </main>
   )
 }
