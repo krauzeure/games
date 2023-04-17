3ed46c67e6700593e9f748f132b3101ede3bed60
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-04-17 18:45:46 +0200

    Adding the other app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Homepage from './Pages/Homepage/Homepage'
+import Games from './Pages/Games/Games'
+import Help from './Pages/Help/Help'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className='App'>
       <Routes>
         <Route path="" element={<Homepage />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </div>
   )
