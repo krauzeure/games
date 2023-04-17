35e39a280bd04065896af818bf2aad4c7d040e6f
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-04-17 18:35:16 +0200

    Integrating the base structure for the homepage

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
 import './Homepage.css'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <main className="homepage-main">
+        <h1>Jeux en ligne</h1>
+        <p>Venez découvrir nos mini-jeux en ligne. Les jeux sont gratuits et sont accessibles sans inscription !</p>
+        <div className="link">Découvrir les jeux</div>
+    </main>
   )
 }
