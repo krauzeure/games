f6dbedef05422bd36de19a3274e4fc88a5036a6c
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-04-18 21:16:54 +0200

    Adding the game explanation

--- src/Pages/Games/Memory/Memory.tsx
+++ src/Pages/Games/Memory/Memory.tsx
@@ -2,6 +2,14 @@ import './Memory.css'
 
 export default function Memory() {
   return (
-    <div>Memory</div>
+    <main className='memory-main'>
+      <section>
+        <h1>Memory</h1>
+        <div className='memory-explanation'>
+          <p>Cliquez sur "Démarrer" et trouvez tous les duos de cartes pour gagner !</p>
+          <button>Démarrer</button>
+        </div>
+      </section>
+    </main>
   )
 }
