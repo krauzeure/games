fe97bf0d9106c1785393a4c1dd969263b6ff38f5
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-05-22 09:25:39 +0200

    Creating a new file for the memory cards data

---
+++ src/Data/memoryCards.tsx
@@ -0,0 +1,108 @@
+import Lahm from '../Data/Images/Memory/Lahm.svg'
+import Schweinsteiger from '../Data/Images/Memory/Schweinsteiger.svg'
+import Kimmich from '../Data/Images/Memory/Kimmich.svg'
+import Goretzka from '../Data/Images/Memory/Goretzka.svg'
+import Robben from '../Data/Images/Memory/Robben.svg'
+import Ribery from '../Data/Images/Memory/Ribery.svg'
+import Muller from '../Data/Images/Memory/Muller.svg'
+import Neuer from '../Data/Images/Memory/Neuer.svg'
+
+export const memoryCards = [
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Goretzka,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Kimmich,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Lahm,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Muller,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Neuer,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Ribery,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Robben,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Schweinsteiger,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Goretzka,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Kimmich,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Lahm,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Muller,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Neuer,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Ribery,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Robben,
+        "isHidden": true
+    },
+    {
+        "isDisabled": false,
+        "name": "react",
+        "image": Schweinsteiger,
+        "isHidden": true
+    }
+
+]
