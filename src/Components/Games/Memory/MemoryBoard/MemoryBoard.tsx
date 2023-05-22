0a1d406053ce19a95cfd9964b9ad67c1dd7ec450
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-05-22 09:25:18 +0200

    Adding a shuffling algorithm and updating the component to match the new data file

--- src/Components/Games/Memory/MemoryBoard/MemoryBoard.tsx
+++ src/Components/Games/Memory/MemoryBoard/MemoryBoard.tsx
@@ -1,12 +1,22 @@
-import { memoryImages } from '../../../../Data/memoryImages'
+import { memoryCards } from '../../../../Data/memoryCards';
 import './MemoryBoard.css'
+
+// Creating a function to shuffle our array
+export const shuffleArray = (array:any) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
 
 export default function MemoryBoard() {
   return (
     <ul className='memory-board'>
-      {memoryImages.map((item, index) => (
+      {memoryCards.map((item, index) => (
         <li key={index}>
-          <img src={item} alt='memory' />
+          <img src={item.image} alt='memory' />
         </li>
       ))}
     </ul>
