78405fef2a32e27e04ba772a0141cd27e8877fb6
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-05-22 11:31:08 +0200

    Adding first card tests

---
+++ src/Components/Games/Memory/Card/Card.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import Card from './Card'
+import Goretzka from '../../../../Data/Images/Memory/Goretzka.svg'
+
+describe('should render correctly', () => {
+  test('should get the alt text passed in props', () => {
+    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} />)
+    const card = screen.getByAltText('Goretzka')
+    expect(card).toBeInTheDocument()
+  })
+
+  test('should be hidden', () => {
+    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} />)
+    const div = screen.getByTestId("card-cover")
+    expect(div).toHaveClass('card-cover')
+  })
+})
