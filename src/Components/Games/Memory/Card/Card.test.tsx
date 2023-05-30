1c929ff50278cdfa96f4aadb3fd3ce518f8ef29c
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-05-30 10:58:42 +0200

    Getting the returnCard function from props and sending back the card number to the parent

--- src/Components/Games/Memory/Card/Card.test.tsx
+++ src/Components/Games/Memory/Card/Card.test.tsx
@@ -4,13 +4,13 @@ import Goretzka from '../../../../Data/Images/Memory/Goretzka.svg'
 
 describe('should render correctly', () => {
   test('should get the alt text passed in props', () => {
-    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} />)
+    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} number={1} turnCard={jest.fn()}/>)
     const card = screen.getByAltText('Goretzka')
     expect(card).toBeInTheDocument()
   })
 
   test('should be hidden', () => {
-    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} />)
+    render(<Card image={Goretzka} alt={'Goretzka'} hidden={true} disabled={false} number={1} turnCard={jest.fn()}/>)
     const div = screen.getByTestId("card-cover")
     expect(div).toHaveClass('card-cover')
   })
